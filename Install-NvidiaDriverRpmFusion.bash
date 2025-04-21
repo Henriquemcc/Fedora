4aab8030bb49b1520471f2e30377015a10c255ad
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-04-21 15:16:07 -0300

    Adding script

---
+++ Install-NvidiaDriverRpmFusion.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Importing function run_as_root
+source RunAsRoot.bash
+
+# Running as root
+run_as_root
+
+# Checking if the computer has a NVIDIA GPU
+if ! lspci | grep -i nvidia; then
+    exit 1
+fi
+
+# Enabling RPM Fusion
+bash ./Enable-RpmFusion.bash
+
+# Installing nvidia driver and nvidia cuda
+dnf install --assumeyes akmod-nvidia
+dnf install --assumeyes xorg-x11-drv-nvidia-cuda
+
+# Enabling kernel module
+echo "nvidia" > "/etc/modules-load.d/nvidia.conf"
