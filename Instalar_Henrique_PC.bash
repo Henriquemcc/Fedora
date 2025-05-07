0a9b3159fde9ad872402e45068b766916b9adc8e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-07 20:48:22 -0300

    Refatorando código

--- Instalar_Henrique_PC.bash
+++ Instalar_Henrique_PC.bash
@@ -44,156 +44,141 @@ function run_as_root() {
     dnf swap --assumeyes --allowerasing ffmpeg-free ffmpeg
 
     # Array de pacotes RPM
-    pacotes_a_serem_instalados=()
-    pacotes_a_serem_removidos=()
+    pacotes_rpm_a_serem_instalados=()
+    pacotes_rpm_a_serem_removidos=()
 
     # Instalando o KeepassXC
-    pacotes_a_serem_instalados+=("keepassxc")
+    pacotes_rpm_a_serem_instalados+=("keepassxc")
 
     # Instalando o Libreoffice
-    pacotes_a_serem_instalados+=("libreoffice-writer")
-    pacotes_a_serem_instalados+=("libreoffice-calc")
-    pacotes_a_serem_instalados+=("libreoffice-impress")
-    pacotes_a_serem_instalados+=("libreoffice-math")
-    pacotes_a_serem_instalados+=("libreoffice-draw")
-    pacotes_a_serem_instalados+=("libreoffice-langpack-pt-BR")
-    pacotes_a_serem_instalados+=("libreoffice-langpack-en")
-    pacotes_a_serem_instalados+=("unoconv")
+    pacotes_rpm_a_serem_instalados+=("libreoffice-writer")
+    pacotes_rpm_a_serem_instalados+=("libreoffice-calc")
+    pacotes_rpm_a_serem_instalados+=("libreoffice-impress")
+    pacotes_rpm_a_serem_instalados+=("libreoffice-math")
+    pacotes_rpm_a_serem_instalados+=("libreoffice-draw")
+    pacotes_rpm_a_serem_instalados+=("libreoffice-langpack-pt-BR")
+    pacotes_rpm_a_serem_instalados+=("libreoffice-langpack-en")
+    pacotes_rpm_a_serem_instalados+=("unoconv")
 
     # Instalando o Gnome Epiphany
-    pacotes_a_serem_instalados+=("epiphany")
+    pacotes_rpm_a_serem_instalados+=("epiphany")
 
     # Instalando o File Roller
-    pacotes_a_serem_instalados+=("file-roller")
-    pacotes_a_serem_instalados+=("file-roller-nautilus")
+    pacotes_rpm_a_serem_instalados+=("file-roller")
+    pacotes_rpm_a_serem_instalados+=("file-roller-nautilus")
 
     # Instalando a Impressora HP
-    pacotes_a_serem_instalados+=("hplip")
+    pacotes_rpm_a_serem_instalados+=("hplip")
 
     # Instalando o Suporte a arquivos 7zip
-    pacotes_a_serem_instalados+=("p7zip-plugins")
-    pacotes_a_serem_instalados+=("p7zip")
+    pacotes_rpm_a_serem_instalados+=("p7zip-plugins")
+    pacotes_rpm_a_serem_instalados+=("p7zip")
 
     # Instalando o KVM
-    pacotes_a_serem_instalados+=("qemu-kvm")
-    pacotes_a_serem_instalados+=("libvirt")
+    pacotes_rpm_a_serem_instalados+=("qemu-kvm")
+    pacotes_rpm_a_serem_instalados+=("libvirt")
 
     # Instalando os Sistemas de arquivos não nativos do linux
-    # pacotes_a_serem_instalados+=("ntfs-3g") # Usando Enable-Ntfs no lugar
-    # pacotes_a_serem_instalados+=("exfat-utils")
-    pacotes_a_serem_instalados+=("fuse")
-    # pacotes_a_serem_instalados+=("fuse-exfat") # Usando Enable-ExFat no lugar
+    # pacotes_rpm_a_serem_instalados+=("ntfs-3g") # Usando Enable-Ntfs no lugar
+    # pacotes_rpm_a_serem_instalados+=("exfat-utils")
+    pacotes_rpm_a_serem_instalados+=("fuse")
+    # pacotes_rpm_a_serem_instalados+=("fuse-exfat") # Usando Enable-ExFat no lugar
 
     # Instalando as Ferramentas de desenvolvimento
-    pacotes_a_serem_instalados+=("golang")
-    pacotes_a_serem_instalados+=("gcc")
-    pacotes_a_serem_instalados+=("gcc-c++")
-    pacotes_a_serem_instalados+=("dotnet-sdk-5.0")
-    pacotes_a_serem_instalados+=("aspnetcore-runtime-5.0")
-    pacotes_a_serem_instalados+=("dotnet-runtime-5.0")
-    pacotes_a_serem_instalados+=("git")
-    pacotes_a_serem_instalados+=("git-lfs")
-    pacotes_a_serem_instalados+=("android-tools")
-    pacotes_a_serem_instalados+=("libstdc++-devel")
-    pacotes_a_serem_instalados+=("perf")
-    pacotes_a_serem_instalados+=("python3-pip")
+    pacotes_rpm_a_serem_instalados+=("golang")
+    pacotes_rpm_a_serem_instalados+=("gcc")
+    pacotes_rpm_a_serem_instalados+=("gcc-c++")
+    pacotes_rpm_a_serem_instalados+=("dotnet-sdk-5.0")
+    pacotes_rpm_a_serem_instalados+=("aspnetcore-runtime-5.0")
+    pacotes_rpm_a_serem_instalados+=("dotnet-runtime-5.0")
+    pacotes_rpm_a_serem_instalados+=("git")
+    pacotes_rpm_a_serem_instalados+=("git-lfs")
+    pacotes_rpm_a_serem_instalados+=("android-tools")
+    pacotes_rpm_a_serem_instalados+=("libstdc++-devel")
+    pacotes_rpm_a_serem_instalados+=("perf")
+    pacotes_rpm_a_serem_instalados+=("python3-pip")
 
     # Instalando outros programas
-    pacotes_a_serem_instalados+=("stacer")
-    pacotes_a_serem_instalados+=("qt5-qtcharts")
-    pacotes_a_serem_instalados+=("vlc")
-    pacotes_a_serem_instalados+=("libdvdcss") # Não disponível no CentOS ou RHEL
-    pacotes_a_serem_instalados+=("qt5-qtsvg")
-    pacotes_a_serem_instalados+=("youtube-dl")
-    pacotes_a_serem_instalados+=("yt-dlp")
-    pacotes_a_serem_instalados+=("snapd")
-    pacotes_a_serem_instalados+=("flatpak")
-    pacotes_a_serem_instalados+=("qbittorrent")
-    pacotes_a_serem_instalados+=("ffmpeg")
-    pacotes_a_serem_instalados+=("steam")
-    pacotes_a_serem_instalados+=("mokutil")
-    pacotes_a_serem_instalados+=("fdupes")
-    pacotes_a_serem_instalados+=("dconf-editor")
-    pacotes_a_serem_instalados+=("gimp")
-    pacotes_a_serem_instalados+=("gedit")
-    pacotes_a_serem_instalados+=("ImageMagick")
-    pacotes_a_serem_instalados+=("pdftk-java")
-    pacotes_a_serem_instalados+=("wol")
-    pacotes_a_serem_instalados+=("brasero")
-    pacotes_a_serem_instalados+=("nmap")
-    pacotes_a_serem_instalados+=("rclone")
-    pacotes_a_serem_instalados+=("wireshark")
+    pacotes_rpm_a_serem_instalados+=("stacer")
+    pacotes_rpm_a_serem_instalados+=("qt5-qtcharts")
+    pacotes_rpm_a_serem_instalados+=("vlc")
+    pacotes_rpm_a_serem_instalados+=("libdvdcss") # Não disponível no CentOS ou RHEL
+    pacotes_rpm_a_serem_instalados+=("qt5-qtsvg")
+    pacotes_rpm_a_serem_instalados+=("youtube-dl")
+    pacotes_rpm_a_serem_instalados+=("yt-dlp")
+    pacotes_rpm_a_serem_instalados+=("snapd")
+    pacotes_rpm_a_serem_instalados+=("flatpak")
+    pacotes_rpm_a_serem_instalados+=("qbittorrent")
+    pacotes_rpm_a_serem_instalados+=("ffmpeg")
+    pacotes_rpm_a_serem_instalados+=("steam")
+    pacotes_rpm_a_serem_instalados+=("mokutil")
+    pacotes_rpm_a_serem_instalados+=("fdupes")
+    pacotes_rpm_a_serem_instalados+=("dconf-editor")
+    pacotes_rpm_a_serem_instalados+=("gimp")
+    pacotes_rpm_a_serem_instalados+=("gedit")
+    pacotes_rpm_a_serem_instalados+=("ImageMagick")
+    pacotes_rpm_a_serem_instalados+=("pdftk-java")
+    pacotes_rpm_a_serem_instalados+=("wol")
+    pacotes_rpm_a_serem_instalados+=("brasero")
+    pacotes_rpm_a_serem_instalados+=("nmap")
+    pacotes_rpm_a_serem_instalados+=("rclone")
+    pacotes_rpm_a_serem_instalados+=("wireshark")
 
     # Instalando leitores de epub
-    pacotes_a_serem_instalados+=("calibre")
-    pacotes_a_serem_instalados+=("okular")
+    pacotes_rpm_a_serem_instalados+=("calibre")
+    pacotes_rpm_a_serem_instalados+=("okular")
 
     # Instalando pacotes para reportar erro automaticamente
-    pacotes_a_serem_instalados+=("abrt-desktop")
-    pacotes_a_serem_instalados+=("abrt-java-connector")
+    pacotes_rpm_a_serem_instalados+=("abrt-desktop")
+    pacotes_rpm_a_serem_instalados+=("abrt-java-connector")
 
     # Instalando ferramentas de segurança
-    pacotes_a_serem_instalados+=("chkrootkit")
-    pacotes_a_serem_instalados+=("lynis")
+    pacotes_rpm_a_serem_instalados+=("chkrootkit")
+    pacotes_rpm_a_serem_instalados+=("lynis")
 
     # Instalando xorg
-    pacotes_a_serem_instalados+=("xorg-x11-server-Xorg")
-    pacotes_a_serem_instalados+=("xorg-x11-xauth")
+    pacotes_rpm_a_serem_instalados+=("xorg-x11-server-Xorg")
+    pacotes_rpm_a_serem_instalados+=("xorg-x11-xauth")
 
     # Instalando o Draw.io
-    pacotes_a_serem_instalados+=("https://github.com/jgraph/drawio-desktop/releases/download/v26.2.15/drawio-x86_64-26.2.15.rpm")
+    pacotes_rpm_a_serem_instalados+=("https://github.com/jgraph/drawio-desktop/releases/download/v26.2.15/drawio-x86_64-26.2.15.rpm")
 
     # Desinstalando pacotes inúteis
     ## Extensões Gnome Shell
-    pacotes_a_serem_removidos+=("gnome-shell-extension-background-logo")
-    pacotes_a_serem_removidos+=("gnome-shell-extension-window-list")
+    pacotes_rpm_a_serem_removidos+=("gnome-shell-extension-background-logo")
+    pacotes_rpm_a_serem_removidos+=("gnome-shell-extension-window-list")
 
     # Realizando instalações
-    install_rpm_packages pacotes_a_serem_instalados
+    install_rpm_packages pacotes_rpm_a_serem_instalados
 
     # Realizando desinstalações
-    uninstall_rpm_packages pacotes_a_serem_removidos
+    uninstall_rpm_packages pacotes_rpm_a_serem_removidos
   }
 
   # Instala pacotes snap
   function instalar_pacotes_snap() {
 
+    # Obtendo funções install_snap_packages e uninstall_snap_packages
+    source SnapPackageManager.bash
+
     # Instalando snapd
     bash ./Install-Snapd.bash
 
+    # Array de pacotes snap
+    pacotes_snap_a_serem_instalados=()
+
     # Instalando pacotes snap
-    while true; do
-      snap install spotify && break
-    done
+    pacotes_snap_a_serem_instalados+=("spotify")
+    pacotes_snap_a_serem_instalados+=("intellij-idea-community --classic")
+    pacotes_snap_a_serem_instalados+=("pycharm-community --classic")
+    pacotes_snap_a_serem_instalados+=("clion --classic")
+    pacotes_snap_a_serem_instalados+=("flutter --classic")
+    pacotes_snap_a_serem_instalados+=("kotlin --classic")
+    pacotes_snap_a_serem_instalados+=("postman")
+    pacotes_snap_a_serem_instalados+=("webstorm --classic")
 
-    while true; do
-      snap install intellij-idea-community --classic && break
-    done
-
-    while true; do
-      snap install pycharm-community --classic && break
-    done
-
-    while true; do
-      snap install clion --classic && break
-    done
-
-    while true; do
-      snap install flutter --classic && break
-    done
-
-    while true; do
-      snap install kotlin --classic && break
-    done
-    
-    while true; do
-      snap install postman && break
-    done
-    
-    while true; do
-      snap install webstorm --classic && break
-    done
-
+    # Instalando pacotes snap
+    install_snap_packages pacotes_snap_a_serem_instalados
   }
 
   # Instala pacotes flatpak
