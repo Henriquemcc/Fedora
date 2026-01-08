5bd84981c65872c28ccccc80d7de9c0a981780f5
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 11:45:45 -0300

    Adding compatibility with Fedora Silverblue

--- Uninstall-NvidiaCudaToolkit.bash
+++ Uninstall-NvidiaCudaToolkit.bash
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# Importing function run_as_root, get_os_type and get_os_version
+# Importing function run_as_root, get_os_type, get_os_version and uninstall_rpm_package
 source RunAsRoot.bash
 source OsInfo.bash
+source RpmPackageManager.bash
 
 # Running as root
 run_as_root
 
 # Removing Nvidia Cuda
 dnf module reset nvidia-driver
-dnf autoremove --assumeyes nvidia-driver
-dnf autoremove --assumeyes cuda
+uninstall_rpm_package nvidia-driver
+uninstall_rpm_package cuda
 
 # Removing repository
 if [ "$(command -v dnf4)" ]; then
   dnf4 config-manager --disable "cuda-rhel9-x86_64"
-else
+elif [ "$(command -v dnf)" ]; then
   dnf config-manager --disable "cuda-rhel9-x86_64"
+else
+  sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/cuda-rhel9-x86_64.repo
 fi
 
 # Updating grub
