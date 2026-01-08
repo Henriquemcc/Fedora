069f75fb75dd560289da5d8a31998d802fb2a42a
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-07 23:45:46 -0300

    Adicionando chamada ao install_rpm_package no lugar do comando 'dnf install'

--- ConfigurarSshdConfig.bash
+++ ConfigurarSshdConfig.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Importing function run_as_root
+# Importing function run_as_root and install_rpm_package
 source RunAsRoot.bash
+source RpmPackageManager.bash
 
 # Running as root
 run_as_root
 
 # Installing Open SSH Server
-dnf install --assumeyes openssh-server
+install_rpm_package "openssh-server"
 
 # Backing up configuration file
 cp "/etc/ssh/sshd_config" "/etc/ssh/sshd_config.backup.$(date "+%d-%m-%Y_%H:%M:%S")"
