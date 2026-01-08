2510abf47900e969873ef63e883af5fc956af184
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 00:14:40 -0300

    Adding call to install_rpm_package instead of 'dnf install'

--- Install-BalenaEtcher.bash
+++ Install-BalenaEtcher.bash
@@ -3,11 +3,12 @@
 version="2.1.4"
 download_url="https://github.com/balena-io/etcher/releases/download/v${version}/balena-etcher-${version}-1.x86_64.rpm"
 
-# Importing function run_as_root
+# Importing function run_as_root and install_rpm_package
 source RunAsRoot.bash
+source RpmPackageManager.bash
 
 # Running as root
 run_as_root
 
 # Installing Balena Etcher
-dnf install --assumeyes "$download_url"
+install_rpm_package "$download_url"
