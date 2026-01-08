51441241ae9796076d79afeff13c0644e2d6db66
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 11:13:43 -0300

    Adding the installation of Snapd

--- Install-VisualStudioCode.bash
+++ Install-VisualStudioCode.bash
@@ -12,12 +12,20 @@ if [ "$(command -v code)" ]; then
   exit 0
 fi
 
-# Installing Visual Studio Code
+# Installing Visual Studio Code from RPM package
 if [ "$(command -v dnf)" ]; then
   rpm --import https://packages.microsoft.com/keys/microsoft.asc
   sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
   dnf --assumeyes check-update
   dnf --assumeyes install code
-elif [ "$(command -v snap)" ]; then
-  snap install code --classic
+
+# Installing Visual Studio Code from Snap package
+else
+  # Installing Snapd
+  bash ./Install-Snapd.bash
+
+  # Installing Visual Studio Code
+  while true; do
+    snap install code --classic && break
+  done
 fi
