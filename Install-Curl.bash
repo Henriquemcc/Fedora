154a4f4d7c840c0a340ad8e186e1461e0e512a84
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 00:21:15 -0300

    Adding call to install_rpm_package instead of 'dnf install'

--- Install-Curl.bash
+++ Install-Curl.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Importing function run_as_root and install_rpm_package
+source RunAsRoot.bash
+source RpmPackageManager.bash
+
+# Running as root
+run_as_root
+
 if ! [ "$(command -v curl)" ]; then
-    sudo dnf install --assumeyes curl
+  install_rpm_package curl
 fi
