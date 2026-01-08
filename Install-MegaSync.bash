0ec2fdcbf7efd049f5e0be33eb6c22abd6da9ef9
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 08:22:58 -0300

    Adding call to install_rpm_package instead of 'dnf install'.

--- Install-MegaSync.bash
+++ Install-MegaSync.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Importing function run_as_root, get_os_type and get_os_version
+# Importing function run_as_root, get_os_type, get_os_version and install_rpm_package
 source RunAsRoot.bash
 source OsInfo.bash
+source RpmPackageManager.bash
 
 # Running as root
 run_as_root
@@ -13,7 +14,7 @@ ARCHITECTURE="$(uname -m)"
 
 # Installing Mega Sync
 if [ "$(get_os_type)" == "fedora" ]; then
-  dnf install --assumeyes "https://mega.nz/linux/repo/Fedora_${VERSION_ID}/${ARCHITECTURE}/megasync-Fedora_${VERSION_ID}.${ARCHITECTURE}.rpm"
+  install_rpm_package "https://mega.nz/linux/repo/Fedora_${VERSION_ID}/${ARCHITECTURE}/megasync-Fedora_${VERSION_ID}.${ARCHITECTURE}.rpm"
 elif [ "$(get_os_type)" == "centos" ] || [ "$(get_os_type)" == "rhel" ] || [ "$(get_os_type)" == "almalinux" ] || [ "$(get_os_type)" == "ol" ]; then
-  dnf install --assumeyes https://mega.nz/linux/repo/Fedora_39/x86_64/megasync-Fedora_39.x86_64.rpm
+  install_rpm_package https://mega.nz/linux/repo/Fedora_39/x86_64/megasync-Fedora_39.x86_64.rpm
 fi
