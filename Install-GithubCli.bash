6215fd5b0dda6d0eb65fd8112996b684fc4c43e6
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 07:36:54 -0300

    - Adding call to install_rpm_package instead of 'dnf install'. - Adding repository to Fedora Silverblue.

--- Install-GithubCli.bash
+++ Install-GithubCli.bash
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# Importing function run_as_root
+# Importing function run_as_root and install_rpm_package
 source RunAsRoot.bash
+source RpmPackageManager.bash
 
 # Running as root
 run_as_root
 
 # Installing requirements
-dnf install  --assumeyes 'dnf-command(config-manager)'
-dnf install --assumeyes git
-dnf install --assumeyes git-lfs
+install_rpm_package 'dnf-command(config-manager)'
+install_rpm_package git
+install_rpm_package git-lfs
 
 # Adding GitHub Cli repository
 if [ "$(command -v dnf4)" ]; then
   dnf4 config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo --assumeyes
-else
+elif [ "$(command -v dnf)" ]; then
   dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo --assumeyes
+else
+  curl -o /etc/yum.repos.d/gh-cli.repo https://cli.github.com/packages/rpm/gh-cli.repo
 fi
 
 # Installing GitHub Cli.
-dnf install --assumeyes gh
+install_rpm_package --assumeyes gh
