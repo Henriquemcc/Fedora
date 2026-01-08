238b707d77697abc80e0fec07d85511e4ba5fdac
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 07:39:36 -0300

    Adding call to install_rpm_package instead of 'dnf install'.

--- Install-GoogleEarth.bash
+++ Install-GoogleEarth.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Importing function run_as_root
+# Importing function run_as_root and install_rpm_package
 source RunAsRoot.bash
+source RpmPackageManager.bash
 
 # Running as root
 run_as_root
 
 # Installing Google Earth
-dnf install --assumeyes https://dl.google.com/dl/earth/client/current/google-earth-pro-stable-current.x86_64.rpm
+install_rpm_package https://dl.google.com/dl/earth/client/current/google-earth-pro-stable-current.x86_64.rpm
