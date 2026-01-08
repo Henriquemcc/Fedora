31975f20d5ae37be8f3ca4c960f02df5e7d3193b
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 07:38:17 -0300

    Adding call to install_rpm_package instead of 'dnf install'.

--- Install-Glib.bash
+++ Install-Glib.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Importing function run_as_root
+# Importing function run_as_root and install_rpm_package
 source RunAsRoot.bash
+source RpmPackageManager.bash
 
 # Running as root
 run_as_root
 
 # Installing Glib
-dnf install glib --assumeyes
+install_rpm_package glib
