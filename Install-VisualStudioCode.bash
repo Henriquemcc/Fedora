7d36cd26c5c0dca5761e2fbd1716291d59be938f
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 11:08:58 -0300

    Adding compatibility with Fedora Silverblue

--- Install-VisualStudioCode.bash
+++ Install-VisualStudioCode.bash
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-if ! [ "$(command -v code)" ]; then
-  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-  sudo dnf --assumeyes check-update
-  sudo dnf --assumeyes install code
+# Importing functions run_as_root and uninstall_rpm_package
+source RunAsRoot.bash
+source RpmPackageManager.bash
+
+# Running as root
+run_as_root
+
+# Exiting if Visual Studio Code is already installed
+if [ "$(command -v code)" ]; then
+  exit 0
+fi
+
+# Installing Visual Studio Code
+if [ "$(command -v dnf)" ]; then
+  rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  dnf --assumeyes check-update
+  dnf --assumeyes install code
+elif [ "$(command -v snap)" ]; then
+  snap install code --classic
 fi
