e0bac6ad9fa172697ca6fd04aa0cccc563ff6a3c
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 11:52:01 -0300

    Adding compatibility with Fedora Silverblue

--- Install-DockerEngine.bash
+++ Install-DockerEngine.bash
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Importing function run_as_root and get_os_type
+# Importing functions run_as_root, get_os_type and install_rpm_package
 source RunAsRoot.bash
 source OsInfo.bash
+source RpmPackageManager.bash
 
 # Running as root
 run_as_root
 
 # Installing requirements
-dnf install --assumeyes dnf-plugins-core
+install_rpm_package dnf-plugins-core
+
+# Applying update on Fedora Silverblue
+if [ "$(command -v rpm-ostree)" ]; then
+  rpm-ostree apply-live
+fi
 
 # Getting OS Type
 os_type="$(get_os_type)"
@@ -19,12 +25,14 @@ fi
 # Adding Docker repository
 if [ "$(command -v dnf4)" ]; then
   dnf4 config-manager --assumeyes --add-repo "https://download.docker.com/linux/${os_type}/docker-ce.repo"
-else
+elif [ "$(command -v dnf)" ]; then
   dnf config-manager --assumeyes --add-repo "https://download.docker.com/linux/${os_type}/docker-ce.repo"
+else
+  curl -o "/etc/yum.repos.d/docker-ce.repo" "https://download.docker.com/linux/${os_type}/docker-ce.repo"
 fi
 
 # Installing Docker Engine
-dnf install --allowerasing --assumeyes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+install_rpm_package --allowerasing docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Enabling Docker
 systemctl enable --now docker containerd.service
