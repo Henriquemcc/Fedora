5fe2d8671e5a7966f4f3ab5bb296b0c5ffbe4e23
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-08 00:13:49 -0300

    Adding call to install_rpm_package instead of 'dnf install'

--- Install-Anaconda.bash
+++ Install-Anaconda.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Importing function run_as_root and install_rpm_package
+source RunAsRoot.bash
+source RpmPackageManager.bash
+
 if ! [ "$(command -v anaconda)" ]; then
-  sudo dnf install --assumeyes anaconda
+  install_rpm_package anaconda
 fi
