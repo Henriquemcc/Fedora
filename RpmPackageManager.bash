f31a4d6d1206a40b4ede7e4fdcd1c4bb29608c0a
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-07 23:41:07 -0300

    Creating function install_rpm_package

--- RpmPackageManager.bash
+++ RpmPackageManager.bash
@@ -4,14 +4,19 @@ function install_rpm_packages() {
   local array_name="$1[@]"
   local rpm_package_to_be_installed=("${!array_name}")
   for package in "${rpm_package_to_be_installed[@]}" ; do
-    if [ "$(command -v dnf)" ]; then
-      dnf install --assumeyes "$package"
-    elif [ "$(command -v yum)" ]; then
-      yum install --assumeyes "$package"
-    elif [ "$(command -v rpm-ostree)" ]; then
-      rpm-ostree install --assumeyes "$package"
-    fi
+    install_rpm_package "$package"
   done
+}
+
+function install_rpm_package() {
+  local package="$1"
+  if [ "$(command -v dnf)" ]; then
+    dnf install --assumeyes "$package"
+  elif [ "$(command -v yum)" ]; then
+    yum install --assumeyes "$package"
+  elif [ "$(command -v rpm-ostree)" ]; then
+    rpm-ostree install --assumeyes "$package"
+  fi
 }
 
 function uninstall_rpm_packages() {
