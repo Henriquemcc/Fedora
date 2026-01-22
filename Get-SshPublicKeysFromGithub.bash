f4230ba908f70352bc2e2156e0dd90a5eebdc44b
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-01-22 04:40:10 -0300

    Adding script

---
+++ Get-SshPublicKeysFromGithub.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Getting GITHUB_USERNAME environment variable
+source .venv
+
+# Downloading ssh public keys
+curl "https://github.com/${GITHUB_USERNAME}.keys" >> ~/.ssh/authorized_keys
+
+# Defining permissions
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
