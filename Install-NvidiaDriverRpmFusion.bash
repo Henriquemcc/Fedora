c3bf75efcb36a4bec2a2b4aaa116ec9ac0de6d0e
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-02-04 21:08:34 -0300

    Replacing Nvidia proprietary driver with the open source one

--- Install-NvidiaDriverRpmFusion.bash
+++ Install-NvidiaDriverRpmFusion.bash
@@ -16,7 +16,7 @@ fi
 bash ./Enable-RpmFusion.bash
 
 # Installing nvidia driver and nvidia cuda
-install_rpm_package --allowerasing akmod-nvidia
+install_rpm_package --allowerasing akmod-nvidia-open
 install_rpm_package --allowerasing xorg-x11-drv-nvidia-cuda
 
 # Enabling kernel module
